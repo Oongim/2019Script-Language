f0e91519e259196e385e789e789b48939d4b943b
KDK(NoteBook) <rkdqmffor@gmail.com>
2019-05-17 16:47:52 +0900

    OptionFrame Wizet RePosition
    옵션프레임의 위젯들 위치를 다시 조정하고 옵션프레임의 크기로만 나오게 설정

--- merged source/MainFrame.py
+++ merged source/MainFrame.py
@@ -9,7 +9,7 @@ class MainFrame:
         self.window = Window()
         self.buildNotebook()
 
-        self.test_frame = OptionFrame(self.notebook.getFrame("test_1"), 800, 500)
+        self.test_frame = OptionFrame(self.notebook.getFrame("test_1"), self.tab1Viewport.right, self.tab1Viewport.height)
         self.window.mainloop()
 
     def buildNotebook(self):
@@ -17,7 +17,7 @@ class MainFrame:
         self.notebook = Notebook(self.window)
         # self.tabs.addTab(self.window, self.window.viewport, "test_1")
         # self.tabs.addTab(self.window, self.window.viewport, "test_2")
-        self.tab1Viewport = Viewport(0, 0, 800, 500)
+        self.tab1Viewport = Viewport(0, 0, 300, 500)
         tab2Viewport = Viewport(0, 0, 500, 300)
 
 
--- merged source/SearchFrame.py
+++ merged source/SearchFrame.py
@@ -5,7 +5,7 @@ from tkinter import font
 
 #Search프레임 관리
 class Search:
-    def __init__(self,window=tkinter.Tk(),width=800,height=600):
+    def __init__(self,window, width=300, height=300):
         self.width=width
         self.height=height
         window.geometry(str(self.width)+"x"+str(self.height))
@@ -21,35 +21,35 @@ class OptionFrame(tkinter.Frame):
         minWidth=40
 
         self.optionList=[["검색 옵션"],
-                    ["주택 종류",      "",      '',      tkinter.BooleanVar()  ,("test","test","test")],
-                    ["동",            "",      '',      tkinter.BooleanVar()  ,("test","test","test")],
-                    ["월세",         "최소",   '',       tkinter.BooleanVar()],
-                    ["",            "최대",   '',       tkinter.BooleanVar()],
-                    ["보증금",       "최소",   '',       tkinter.BooleanVar()],
-                    ["",            "최대",   '',       tkinter.BooleanVar()],
-                    ["건물 면적",    "최소",   '',       tkinter.BooleanVar()],
-                    ["",            "최대",   '',       tkinter.BooleanVar()]
-                    ]
+                         ["주택 종류",      "",      '',      tkinter.BooleanVar()  ,("test","test","test")],
+                         ["동",            "",      '',      tkinter.BooleanVar()  ,("test","test","test")],
+                         ["월세",         "최소",   '',       tkinter.BooleanVar()],
+                         ["",            "최대",   '',       tkinter.BooleanVar()],
+                         ["보증금",       "최소",   '',       tkinter.BooleanVar()],
+                         ["",            "최대",   '',       tkinter.BooleanVar()],
+                         ["건물 면적",    "최소",   '',       tkinter.BooleanVar()],
+                         ["",            "최대",   '',       tkinter.BooleanVar()]
+                         ]
 
         #처음 검색옵션만 예외로 위치 설정
         TempFont = tkinter.font.Font(window, size=20, weight='bold', family='Consolas')
-        tkinter.Label(window, font=TempFont, text=self.optionList[0][0]).place(x=minWidth + 30, y=heightRate+ minHeight, anchor="s")
-        tkinter.Button(window, text="검색", command=self.Search).place(x=width / 4, y=heightRate - 13)
+        tkinter.Label(window, font=TempFont, text=self.optionList[0][0]).place(x=width/4/2 + 30, y=heightRate + minHeight, anchor="s")
+        tkinter.Button(window, text="검색", command=self.Search).place(x=width-20, y=heightRate+13 ,anchor="se")
         for i in range(1,len(self.optionList)):
             #0번 인덱스가 있을 경우 옵션 이름 세팅
             if self.optionList[i][0]:
-                tkinter.Label(window, text=self.optionList[i][0]).place(x=minWidth, y=heightRate * (i + 1) + minHeight, anchor="s")
+                tkinter.Label(window, text=self.optionList[i][0]).place(x=width/4/2, y=heightRate * (i + 1) + minHeight, anchor="s")
             # 1번 인덱스에 최소 최대 가 있을 경우 이름 세팅과 2번에 Entry세팅
             if self.optionList[i][1]:
-                tkinter.Label(window, text=self.optionList[i][1]).place(x=width / 4 / 2, y=heightRate * (i + 1) + minHeight, anchor="s")
-                self.optionList[i][2]=tkinter.Entry(window, width=int((width / 4 + 35 - width / 4 / 3 * 2) / 10), state='disabled')
+                tkinter.Label(window, text=self.optionList[i][1]).place(x=width / 3, y=heightRate * (i + 1) + minHeight, anchor="s")
+                self.optionList[i][2]=tkinter.Entry(window, width=int((width + 35 - width / 3 * 2) / 10), state='disabled')
             #1번 인덱스가 빌 경우 2번에 콤보박스 세팅
             else:
-                self.optionList[i][2]=(ttk.Combobox(window, width=int((width / 4 + 15 - width / 4 / 3 * 2) / 10), textvariable=str,state='disabled'))
+                self.optionList[i][2]=(ttk.Combobox(window, width=int((width + 15 - width / 3 * 2) / 10), textvariable=str,state='disabled'))
                 self.optionList[i][2]['value']=self.optionList[i][4]
-            self.optionList[i][2].place(x=width / 4 + 35, y=heightRate * (i + 1) + minHeight, anchor="se")
+            self.optionList[i][2].place(x=width-20, y=heightRate * (i + 1) + minHeight, anchor="se")
             #체크 박스 세팅
-            self.optionList[i].append(tkinter.ttk.Checkbutton(window,variable= self.optionList[i][3], command=self.activeEntry).place(x=width / 4 / 3 * 2,
+            self.optionList[i].append(tkinter.ttk.Checkbutton(window,variable= self.optionList[i][3], command=self.activeEntry).place(x=width / 2 ,
                                                                         y=heightRate * (i+1) + minHeight, anchor="s"))
     def Search(self):
         for i in range(1, len(self.optionList)):
@@ -67,8 +67,5 @@ class OptionFrame(tkinter.Frame):
                 else:
                     self.optionList[i][2]['state'] = 'disabled'
 
-
-
-Search()
 
 
