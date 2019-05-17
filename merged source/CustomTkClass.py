2cf4f1932b652956d5053da620408c0dd4e98051
Note_book <43131676+kyc1096@users.noreply.github.com>
2019-05-17 15:29:57 +0900

    CustomTkClass Refactoring
    소스 파일 이름 수정 :
    TkinterManager->CustomTkClass
    
    tkinter 클래스의 유도 클래스 디자인으로 코드 리팩토링

---
+++ merged source/CustomTkClass.py
@@ -0,0 +1,103 @@
+import tkinter
+import tkinter.ttk
+import tkinter.font
+from Error import *
+
+
+
+class Viewport:
+    def __init__(self, left=0, top=0, width=0, height=0):
+        self.left = left
+        self.top = top
+        self.right = left+width
+        self.bottom = top+height
+        self.width = width
+        self.height = height
+
+
+class Window(tkinter.Tk):
+    def __init__(self, left=200, top=200, width=1280, height=720):
+        super(Window, self).__init__()
+        self.resizable(False, False)
+        self.viewport = Viewport()
+        self.resize(width=width, height=height, left=left, top=top)
+
+
+    def resize(self, width, height, left=None, top=None):
+        if not left == None:
+            self.viewport.left = left
+        if not top == None:
+            self.viewport.top = top
+        self.viewport.width = width
+        self.viewport.height = height
+        self.geometry \
+            ("{width}x{height}+{left}+{top}".format(width=self.viewport.width, height=self.viewport.height,
+                                                    left=self.viewport.left, top=self.viewport.top))
+
+
+class Notebook(tkinter.ttk.Notebook):
+    upperGab = 15
+    def __init__(self, window):
+        self.window = window
+        super(Notebook, self).__init__(window)
+        self.pack()
+        self.bind("<<NotebookTabChanged>>", Notebook.funcOnTabChanged)
+        self.tabStackCount = 0
+        self.dictTabFrame = {}
+        self.dictTabIndex = {}
+        self.dictTabFrameFromIndex = {}
+
+
+    @staticmethod
+    def setTabSytle():
+        style = tkinter.ttk.Style()
+        style.theme_create("tabStyle", parent="alt", settings={
+            "TNotebook": {"configure": {"tabmargins": [2, 5, 2, 0]}},
+            "TNotebook.Tab": {"configure": {"padding": [10, 4]}}})
+        style.theme_use("tabStyle")
+
+
+    def addTab(self, window, viewport, tabName=""):
+        if tabName in self.dictTabIndex.keys():
+            raise AlreadyInKeysOfDict("{str} is already in keys of dictTabID".format(str=tabName))
+        tabFrame = TabFrame(window, viewport)
+        self.add(tabFrame, text=tabName)
+        self.dictTabFrame[tabName] = tabFrame
+        self.dictTabIndex[tabName] = str(self.tabStackCount)
+        self.dictTabFrameFromIndex[str(self.tabStackCount)] = tabFrame
+        self.tabStackCount = self.tabStackCount + 1
+
+    def resize(self, width, height):
+        self.configure(width=width, height=height)
+
+    def getFrame(self, strKey):
+        return self.dictTabFrame[strKey]
+
+    def getFrameFromID(self, tabID):
+        for frame in self.dictTabFrame.values():
+            if frame._w == tabID:
+                return frame
+        return None
+
+    def selectTab(self, strKey):
+        self.select(self.dictTabIndex[strKey])
+
+    @staticmethod
+    def funcOnTabChanged(tab):
+        tab.widget.getFrameFromID(tab.widget.select()).onVisibility()
+
+
+
+class Frame(tkinter.Frame):
+    def __init__(self, window, viewport):
+        self.window = window
+        self.viewport = viewport
+        super(Frame, self).__init__(window, width=viewport.width, height=viewport.height)
+
+class TabFrame(Frame):
+    def __init__(self, window, viewport):
+        super(TabFrame, self).__init__(window, viewport)
+
+    def onVisibility(self):
+        # 노트북의 크기 전환은 할 필요없음. 자동으로 되는 듯
+        self.window.resize(width=self.viewport.width, height=self.viewport.height+Notebook.upperGab)
--- merged source/MainFrame.py
+++ merged source/MainFrame.py
@@ -1,21 +1,28 @@
 
-from TkinterManager import*
+from CustomTkClass import*
 from SearchFrame import *
 
 
 
 class MainFrame:
     def __init__(self):
-        self.windowManager = WindowManager()
+        self.window = Window()
+        self.buildNotebook()
 
-        TabManager.setTabSytle()
-        self.tabs = TabManager(self.windowManager.self, self.windowManager.viewport)
-        self.tabs.addTab(self.windowManager.self, "test_1")
-        self.tabs.addTab(self.windowManager.self, "test_2")
-        # self.test_frame = OptionFrame(self.tabs.getFrame("test_1"), 800, 600)
-        self.windowManager.mainloop()
+        self.test_frame = OptionFrame(self.notebook.getFrame("test_1"), 800, 600)
+        self.window.mainloop()
+
+    def buildNotebook(self):
+        Notebook.setTabSytle()
+        self.notebook = Notebook(self.window)
+        # self.tabs.addTab(self.window, self.window.viewport, "test_1")
+        # self.tabs.addTab(self.window, self.window.viewport, "test_2")
+        tab1Viewport = Viewport(0, 0, 800, 600)
+        tab2Viewport = Viewport(0, 0, 500, 300)
 
 
+        self.notebook.addTab(self.window, tab1Viewport, "test_1")
+        self.notebook.addTab(self.window, tab2Viewport, "test_2")
 
 
 MainFrame()
