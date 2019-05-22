9cbd442710e30221f7c8eb026b1de4296340acb8
Note_book <43131676+kyc1096@users.noreply.github.com>
2019-05-22 13:13:25 +0900

    SearchFrame - 상속 통합
    SearchFrame - 상속 통합
    CustomTkClass :: Frame 기반으로 통합함.

--- merged source/MainFrame.py
+++ merged source/MainFrame.py
@@ -1,7 +1,5 @@
-
 from CustomTkClass import*
 from SearchFrame import *
-
 
 
 class MainFrame:
@@ -9,7 +7,14 @@ class MainFrame:
         self.window = Window()
         self.buildNotebook()
 
-        self.test_frame = OptionFrame(self.notebook.getFrame("test_1"), self.tab1Viewport.right, self.tab1Viewport.height)
+
+        optionFrame = OptionFrame(self.notebook.getFrame("test_1"),
+                                    Viewport(0, 0,
+                                      self.notebook.getFrame("test_1").viewport.right-500,
+                                      self.notebook.getFrame("test_1").viewport.height))
+        optionFrame.grid(row=0, column=0)
+
+
         self.window.mainloop()
 
     def buildNotebook(self):
@@ -17,11 +22,11 @@ class MainFrame:
         self.notebook = Notebook(self.window)
         # self.tabs.addTab(self.window, self.window.viewport, "test_1")
         # self.tabs.addTab(self.window, self.window.viewport, "test_2")
-        self.tab1Viewport = Viewport(0, 0, 300, 500)
+        tab1Viewport = Viewport(0, 0, 800, 500)
         tab2Viewport = Viewport(0, 0, 500, 300)
 
 
-        self.notebook.addTab(self.window, self.tab1Viewport, "test_1")
+        self.notebook.addTab(self.window, tab1Viewport, "test_1")
         self.notebook.addTab(self.window, tab2Viewport, "test_2")
 
 
--- merged source/SearchFrame.py
+++ merged source/SearchFrame.py
@@ -1,6 +1,4 @@
-import tkinter
-from tkinter import ttk
-from tkinter import font
+from CustomTkClass import*
 
 
 #Search프레임 관리
@@ -9,13 +7,15 @@ class Search:
         self.width=width
         self.height=height
         window.geometry(str(self.width)+"x"+str(self.height))
-        frame=OptionFrame(window,width,height)
-
+        frame=OptionFrame(window,Viewport(0,0,width, height))
         window.mainloop()
 
 #옵션의 프레임
-class OptionFrame(tkinter.Frame):
-    def __init__(self,window,width,height):
+class OptionFrame(Frame):
+    def __init__(self,window,viewport):
+        super(OptionFrame, self).__init__(window, viewport)
+        width = viewport.width
+        height = viewport.height
         heightRate=height/10
         minHeight=20
         minWidth=40
@@ -45,7 +45,7 @@ class OptionFrame(tkinter.Frame):
                 self.optionList[i][2]=tkinter.Entry(window, width=int((width + 35 - width / 3 * 2) / 10), state='disabled')
             #1번 인덱스가 빌 경우 2번에 콤보박스 세팅
             else:
-                self.optionList[i][2]=(ttk.Combobox(window, width=int((width + 15 - width / 3 * 2) / 10), textvariable=str,state='disabled'))
+                self.optionList[i][2]=(tkinter.ttk.Combobox(window, width=int((width + 15 - width / 3 * 2) / 10), textvariable=str,state='disabled'))
                 self.optionList[i][2]['value']=self.optionList[i][4]
             self.optionList[i][2].place(x=width-20, y=heightRate * (i + 1) + minHeight, anchor="se")
             #체크 박스 세팅
@@ -66,6 +66,7 @@ class OptionFrame(tkinter.Frame):
                         self.optionList[i][2]['state'] = 'normal'
                 else:
                     self.optionList[i][2]['state'] = 'disabled'
+
 
 
 
