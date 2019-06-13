86ca6f78196654fc32e0ca9e61a8561be35f8ce9
Note_book <43131676+kyc1096@users.noreply.github.com>
2019-06-13 10:38:45 +0900

    Add  setup 배포판
    Add  setup 배포판

---
+++ merged source/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(name='KPU-Bang',
+      version='1.0',
+      py_modules=['CustomTkClass',
+                  'DataClass',
+                  'Error',
+                  'GmailAndXMLSaveLoadFrame',
+                  'GoogleMap',
+                  'GraphFrame',
+                  'InformationFrame',
+                  'logoLoading',
+                  'MainFrame',
+                  'ReadingDataFromXML',
+                  'SearchFrame',
+                  'SettingFrame',
+                  'spam',
+                  'Telegram',
+                  'UtilityDOM'
+                  ]
+      )
